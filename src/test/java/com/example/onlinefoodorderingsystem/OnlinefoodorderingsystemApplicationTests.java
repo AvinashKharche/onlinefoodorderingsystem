ae2b904a88bc8b2b331077854cc4976aca6380bc
Avinash Kharche <avinashkharche152@gmail.com>
2021-10-24 18:34:26 -0400

    Adding Java backend and vue-frontend project structure

---
+++ src/test/java/com/example/onlinefoodorderingsystem/OnlinefoodorderingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinefoodorderingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinefoodorderingsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
